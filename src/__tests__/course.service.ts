004706afe0f526f5251e4114d5146aaefd809fb1
nataneb <you@example.com>
2020-03-27 16:13:48 -0300

    change unit test to integration test

--- src/__tests__/course.service.ts
+++ src/__tests__/course.service.ts
@@ -1,28 +1,48 @@
 import CourseService from '../services/CourseService'
-const typeorm = require('typeorm')
-describe('Course Service', () => {
-  describe('getUsers', () => {
-    it('should return courses', async () => {
-      const mock = {
-        select: jest.fn().mockReturnThis(),
-        from: jest.fn().mockReturnThis(),
-        getMany: jest.fn().mockResolvedValue('0x0'),
-        relation: jest.fn().mockReturnThis(),
-        offset: jest.fn().mockReturnThis(),
-        limit: jest.fn().mockReturnThis(),
-        leftJoinAndSelect: jest.fn().mockReturnThis(),
-        createQueryBuilder: jest.fn().mockReturnThis()
-      }
-      typeorm.getRepository = jest.fn().mockReturnValue(mock)
+import { createConnection, getRepository, getConnection } from 'typeorm'
+import * as typeorm from 'typeorm'
+import { Course } from '../entity/Course'
+import { User } from '../entity/User'
+import { CourseModule } from '../entity/CourseModules'
+import { Media } from '../entity/Media'
+import { Video } from '../entity/Video'
+import { FileUpload } from 'graphql-upload'
+import { createReadStream } from 'fs'
+import { join } from 'path'
 
-      const courses = await CourseService.getCourses(1, 1)
-      expect(courses).toBe('0x0')
+describe('Course Service', () => {
+  beforeEach(async () => {
+    try {
+      await getConnection().close() // Yep. it is a hack :)
+    } catch {
+
+    }
+    return createConnection({
+      type: 'sqlite',
+      database: ':memory:',
+      dropSchema: true,
+      entities: [Course, User, CourseModule, Media, Video],
+      synchronize: true,
+      logging: false
     })
   })
+  describe('getUsers', () => {
+    it('should return one course', async () => {
+      await getRepository(User).save(<User>{ username: 'admin', password: 'password' })
 
-  describe('findById', () => {
-    it('should return a course', () => {
+      const course = await CourseService.createCourse(1, 'asd', 12.32, 'desc', <FileUpload>{ createReadStream: () => createReadStream(join(__dirname, './file.jpg')), filename: 'file.jpg' })
+      expect(await getRepository(Course).findOne(course.id)).toEqual(expect.objectContaining({ id: 1, name: 'asd', price: 12.32 }))
+    })
+    it('should return one course should have right media', async () => {
+      await getRepository(User).save(<User>{ username: 'admin', password: 'password' })
 
+      const course = await CourseService.createCourse(1, 'asd', 12.32, 'desc', <FileUpload>{ createReadStream: () => createReadStream(join(__dirname, './file.jpg')), filename: 'file.jpg' })
+      expect(getRepository(Course).findOne({ where: { id: course.id }, relations: ['modules', 'thumbnail'] })).resolves.toEqual(expect.objectContaining({
+        thumbnail: expect.objectContaining({
+          id: 1,
+          name: course.thumbnail.name
+        })
+      }))
     })
   })
 })
